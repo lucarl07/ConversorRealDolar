644f02adac02abda4135a37659e711f4b892bf45
lucarl07 <junitinho2007@gmail.com>
2024-05-25 15:26:39 -0300

    Fix padding (by removing edge-to-edge display) and formatting

--- app/src/main/java/com/example/conversorrealdolar/MainActivity.kt
+++ app/src/main/java/com/example/conversorrealdolar/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.conversorrealdolar
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -20,8 +19,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        enableEdgeToEdge() // Maximiza o espaço da aplicação no dispositivo
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
