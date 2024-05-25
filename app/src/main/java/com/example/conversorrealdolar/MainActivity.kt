9f4058f5c9b75733e9b9b66f693a0bd422d417ec
lucarl07 <junitinho2007@gmail.com>
2024-05-25 16:16:50 -0300

    Create function showToastXML + rearrange code on MainActivity.kt

--- app/src/main/java/com/example/conversorrealdolar/MainActivity.kt
+++ app/src/main/java/com/example/conversorrealdolar/MainActivity.kt
@@ -12,6 +12,31 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private fun showToastXML(path: Int, duration: String) {
+        val toastLength = when (duration) {
+            "short" -> Toast.LENGTH_SHORT
+            "long" -> Toast.LENGTH_LONG
+            else -> return Toast.makeText(applicationContext,
+                "Error on MainActivity.kt > showToastText: Invalid \"length\" parameter. ",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+        Toast.makeText(applicationContext, getString(path), toastLength).show()
+    }
+
+    private fun writeConvertedValue(rate: Double, symbol: Char) {
+        val reais = binding.editBRL.text.toString()
+
+        if (reais.isNotEmpty()) {
+            val currency = Math.round((reais.toDouble() * rate) * 100.0) / 100.0
+            val result = "O valor corresponde a $symbol $currency"
+            binding.textResultado.text = result
+        } else {
+            showToastXML(R.string.aviso_caso_vazio, "short")
+        }
+    }
+
     @SuppressLint("DefaultLocale")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,20 +57,6 @@ class MainActivity : AppCompatActivity() {
         }
         binding.btnPesoArg.setOnClickListener {
             writeConvertedValue(170.0, '$')
-        }
-    }
-
-    private fun writeConvertedValue(rate: Double, symbol: Char) {
-        val reais = binding.editBRL.text.toString()
-
-        if (reais.isNotEmpty()) {
-            val currency = Math.round((reais.toDouble() * rate) * 100.0) / 100.0
-            val result = "O valor corresponde a $symbol $currency"
-            binding.textResultado.text = result
-        } else {
-            Toast.makeText(applicationContext,
-                getString(R.string.aviso_caso_vazio),
-                Toast.LENGTH_SHORT).show()
         }
     }
 }
